96be08b5165d56e9b473062423b2acf53d2608f8
Diego Ivan Gonzalez Loeza <diegonzax@gmail.com>
2015-05-01 17:42:47 -0500

    Plantilla inicial

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php 
+    
+?>
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+ 
+  <meta http-equiv = "Content-Type" content="text/html"; charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+  <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+  <title>EL Blog del HTML5</title>
+  <link rel="stylesheet" type= "text/css" href="css/styles.css" />
+ 
+ 
+ </head>
+
+
+
+<body>
+    
+    
+    <div class="container"></div>
+    <header><p>El Blog del HTML5</p></header>
+    <div class="leftColumn">
+    
+    <nav class="prMenu">
+        <ul>
+            <li><a>Opcion 1</a></li>
+            <li><a>Opcion 2</a></li>
+            <li><a>Opcion 3</a></li>
+            <li><a>Opcion 4</a></li>
+        </ul>
+    </nav>
+        
+    <nav class="snMenu">
+        <span>Siguenos en:</span>
+        <ul>
+            <li><a>Facebook</a></li>
+            <li><a>Twitter</a></li>
+            <li><a>Youtube</a></li>
+        </ul>
+    </nav>
+    
+    </div>
+    
+    <div class="centerColumn">  
+   
+     
+
+        
+    <!--<footer>© 2015 Copyright Mi Cuponera. Todos los derechos reservados. </footer>-->
+    </div>
+
+    <div class="rightColumn">
+    <nav class="snMenu">
+        <ul>
+            <li><a>Facebook LIKE</a></li>
+            <li><a>Twits</a></li>
+        </ul>
+    </nav>
+    
+    
+    </div>
+    
+    
+    
+    
+    
+			
+</body>
+</html>
