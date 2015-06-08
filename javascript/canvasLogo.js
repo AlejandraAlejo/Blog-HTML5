c4382e5db09115205912b43efaeabcc51838f865
Alejandra Alejo <ale_alejor@hotmail.com>
2015-06-08 03:44:03 -0500

    javascript

--- javascript/canvasLogo.js
+++ javascript/canvasLogo.js
@@ -2,79 +2,74 @@ window.onload = function(){
     dibujaLogo();
 };
 
+
 function dibujaLogo(){
-    var canvas = document.getElementById('logohtml5');
+    var canvas = document.getElementById('logo-html5');
     var ctx = canvas.getContext('2d');
     
-    ctx.font = "bolder 15px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("HTML", 30, 20);
-    ctx.translate(0,23);
-    
-    // Escudo de fondo naranja
     ctx.fillStyle = "#E34C26";
     ctx.beginPath();
-    ctx.moveTo(20, 65);
-    ctx.lineTo(15, 0);
-    ctx.lineTo(85, 0);
-    ctx.lineTo(80, 65);
-    ctx.lineTo(51, 74);
+    ctx.moveTo(39, 250);
+    ctx.lineTo(17, 0);
+    ctx.lineTo(262, 0);
+    ctx.lineTo(239, 250);
+    ctx.lineTo(139, 278);
     ctx.closePath();
     ctx.fill();
- 
-    // naranja parte de la derecha
+
+    // naranja iluminado de la parte de la derecha
     ctx.fillStyle = "#F06529";
     ctx.beginPath();
-    ctx.moveTo(51, 71);
-    ctx.lineTo(77, 63);
-    ctx.lineTo(82, 3);
-    ctx.lineTo(51, 3);
+    ctx.moveTo(139, 257);
+    ctx.lineTo(220, 234);
+    ctx.lineTo(239, 20);
+    ctx.lineTo(139, 20);
     ctx.closePath();
     ctx.fill();
- 
-    //Parte izquierda del número 5 
+
+    // Gris claro del 5 de su parte izquierda
     ctx.fillStyle = "#EBEBEB";
     ctx.beginPath();
-    ctx.moveTo(51, 25);
-    ctx.lineTo(39, 25);
-    ctx.lineTo(38, 17);
-    ctx.lineTo(51, 17);
-    ctx.lineTo(51, 7);
-    ctx.lineTo(27, 7);
-    ctx.lineTo(29, 35);
-    ctx.lineTo(51, 35);
+    ctx.moveTo(139, 113);
+    ctx.lineTo(98, 113);
+    ctx.lineTo(96, 82);
+    ctx.lineTo(139, 82);
+    ctx.lineTo(139, 51);
+    ctx.lineTo(62, 51);
+    ctx.lineTo(70, 144);
+    ctx.lineTo(139, 144);
     ctx.closePath();
     ctx.fill();
- 
+
     ctx.beginPath();
-    ctx.moveTo(51, 53);
-    ctx.lineTo(42, 50);
-    ctx.lineTo(42, 40);
-    ctx.lineTo(30, 40);
-    ctx.lineTo(32, 60);
-    ctx.lineTo(51, 66);
+    ctx.moveTo(139, 193);
+    ctx.lineTo(105, 184);
+    ctx.lineTo(103, 159);
+    ctx.lineTo(72, 159);
+    ctx.lineTo(76, 207);
+    ctx.lineTo(139, 225);
     ctx.closePath();
     ctx.fill();
- 
-   // Parte derecha del número 5
+
+    // el 5 en lado de la derecha
     ctx.fillStyle = "#FFFFFF";
     ctx.beginPath();
-    ctx.moveTo(51, 25);
-    ctx.lineTo(51, 35);
-    ctx.lineTo(60, 35);
-    ctx.lineTo(60, 50);
-    ctx.lineTo(51, 53);
-    ctx.lineTo(51, 66);
-    ctx.lineTo(70, 60);
-    ctx.lineTo(72, 25);
+    ctx.moveTo(139, 113);
+    ctx.lineTo(139, 144);
+    ctx.lineTo(177, 144);
+    ctx.lineTo(173, 184);
+    ctx.lineTo(139, 193);
+    ctx.lineTo(139, 225);
+    ctx.lineTo(202, 207);
+    ctx.lineTo(210, 113);
     ctx.closePath();
     ctx.fill();
- 
+
     ctx.beginPath();
-    ctx.moveTo(51, 17);
-    ctx.lineTo(51, 7);
-    ctx.lineTo(74, 7);
-    ctx.lineTo(73, 17);
+    ctx.moveTo(139, 51);
+    ctx.lineTo(139, 82);
+    ctx.lineTo(213, 82);
+    ctx.lineTo(216, 51);
     ctx.closePath();
     ctx.fill();
 }
